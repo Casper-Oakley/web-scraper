29fedbe9f8f114cca20708e22b773b0e60d7216d
Casper Oakley <casper_oakley@hotmail.com>
2016-10-27 17:23:55 +0100

    Revert "Engine recurse"

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
 var scraper = require('./scraper');
 
-scraper('http://gocardless.com', 'gocardless.com', function(err, res) {
-  if(err) {
-    console.log('FFUCKED IT' + err);
-  } else {
-    console.log(res);
-  }
-});
+console.log(scraper('https://gocardless.com/', 'gocardless.com', {}));
