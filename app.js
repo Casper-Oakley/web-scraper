6a1edc66ce664f50d55f95ab1f4485137868b510
Casper Oakley <casper_oakley@hotmail.com>
2016-10-28 15:31:46 +0100

    Scraper now checks header file to check mime type before downloading the body

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var scraper = require('./scraper');
+var scraper = require('./scraper'),
+    util    = require('util');
+
+var http = require('http')
+http.globalAgent.maxSockets = Infinity;
 
 scraper('http://gocardless.com', 'gocardless.com', function(err, res) {
+//scraper('https://stallman.org', 'stallman.org', function(err, res) {
   if(err) {
     console.log('FFUCKED IT' + err);
   } else {
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
     console.log(res);
+    console.log(util.inspect(res, {showHidden: false, depth: null}))
   }
 });
